21d152bb8efaa9bab53208230e33ffff49e184d3
Ragavendar <103548730+Ragavendar1@users.noreply.github.com>
2022-06-14 07:43:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ for (var i = 0; i<numberOfDrumButtons; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function (){
   
-  var buttonInnerHTML = this.innerHTML;
+  var buttonInnerHTML = this.=InnerHTML;
 switch (buttonInnerHTML) {
 
   case "w":
