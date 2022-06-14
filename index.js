f7e5bde62bb51c2bd4b8ba6bdf91d27e55b630c4
Ragavendar <103548730+Ragavendar1@users.noreply.github.com>
2022-06-14 14:55:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,41 +18,40 @@ function keyPress(event){
 
 function soundAdd (key){
   switch (key) {
-    case "l":
+    case "w":
     var  kick = new Audio("sounds/kick-bass.mp3");
     kick.play();
       break;
 
-    case "k":
+    case "a":
     var crash = new Audio("sounds/crash.mp3");
     crash.play();
       break;
 
-    case "j":
+    case "s":
     var snare = new Audio("sounds/snare.mp3");
     snare.play();
     break;
 
-    case "w":
+    case "d":
     var tom1 = new Audio("sounds/tom-1.mp3");
     tom1.play();
     break;
 
-    case "a":
+    case "j":
     var tom2 = new Audio("sounds/tom-2.mp3");
     tom2.play();
     break;
 
-    case "s":
+    case "k":
     var tom3 = new Audio("sounds/tom-3.mp3");
     tom3.play();
     break;
 
-    case "d":
+    case "l":
     var tom4 = new Audio("sounds/tom-4.mp3");
     tom4.play();
     break;
-    
     default:
 
   }
