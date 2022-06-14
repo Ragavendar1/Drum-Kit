7a8c835689339622c16ea211cb8863a3b89299b6
Ragavendar <103548730+Ragavendar1@users.noreply.github.com>
2022-06-14 20:29:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
 for (var i = 0; i<numberOfDrumButtons; i++){
 
-document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
 var buttonInnerHTML = this.innerHTML;
-soundAdd(buttonSound);
 switch (buttonInnerHTML) {
 
   case "w":
